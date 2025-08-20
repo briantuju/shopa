5fcc00313b46d6e6325144dbf5c933212bb95182
Brian Tuju <briantuju@gmail.com>
2025-08-20 18:22:00 +0300

    [feature] Admin can now add categories

---
+++ tests/Feature/Filament/Admin/CategoryTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Filament\Resources\Categories\CategoryResource;
+use App\Models\User;
+
+use function Pest\Laravel\actingAs;
+
+beforeEach(function () {
+    $admin = User::factory()->create([
+        'email' => config('customconfig.app.admin_email'),
+    ]);
+
+    actingAs($admin);
+});
+
+test('can access the admin panel for categories', function () {
+    $response = $this->get(CategoryResource::getUrl());
+
+    $response->assertOk();
+});
