fa6ee38155653159ab6a1cb22d62059f7593d5fa
Brian Tuju <briantuju@gmail.com>
2025-08-20 14:41:03 +0300

    [database] Add Brand model with seeder

---
+++ database/seeders/BrandSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Brand;
+use Illuminate\Database\Seeder;
+
+class BrandSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Brand::firstOrCreate(['name' => 'Adidas'], [
+            'description' => 'German multinational corporation, designs and manufactures shoes, clothing and accessories.',
+            'logo' => 'logos/adidas_logo.png',
+        ]);
+
+        Brand::firstOrCreate(['name' => 'Apple'], [
+            'description' => 'American multinational technology company focusing on consumer electronics, software, and online services.',
+            'logo' => 'logos/apple_logo.jpg',
+        ]);
+
+        Brand::firstOrCreate(['name' => 'Nike'], [
+            'description' => 'Global leader in athletic footwear, apparel, equipment, accessories, and services.',
+            'logo' => 'logos/nike_logo.png',
+        ]);
+
+        Brand::firstOrCreate(['name' => 'Samsung'], [
+            'description' => 'South Korean multinational manufacturing conglomerate headquartered in Samsung Digital City, Suwon, South Korea.',
+            'logo' => 'logos/samsung_logo.png',
+        ]);
+
+        Brand::firstOrCreate(['name' => 'Zara'], [
+            'description' => 'Spanish apparel retailer based in Arteixo, Galicia, Spain.',
+            'logo' => 'logos/zara_logo.png',
+        ]);
+    }
+}
