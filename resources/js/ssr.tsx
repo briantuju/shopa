747aa704fa7666793ca94068ef7e2cbb69fb67f3
Brian Tuju <briantuju@gmail.com>
2025-08-23 18:50:19 +0300

    [update] Use layout for ssr components

--- resources/js/ssr.tsx
+++ resources/js/ssr.tsx
@@ -1,17 +1,27 @@
 import { createInertiaApp } from '@inertiajs/react';
 import createServer from '@inertiajs/react/server';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ComponentType, ReactNode } from 'react';
 import ReactDOMServer from 'react-dom/server';
 import { type RouteName, route } from 'ziggy-js';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+type PageComponent = ComponentType & {
+    layout?: (page: ReactNode) => ReactNode;
+};
 
 createServer((page) =>
     createInertiaApp({
         page,
         render: ReactDOMServer.renderToString,
         title: (title) => (title ? `${title} - ${appName}` : appName),
-        resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
+        resolve: async (name) => {
+            const component = (await resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx'))) as { default: PageComponent };
+            component.default.layout = component.default.layout || ((page: ReactNode) => <>{page}</>);
+            return page;
+        },
+        // resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
         setup: ({ App, props }) => {
             /* eslint-disable */
             // @ts-expect-error
